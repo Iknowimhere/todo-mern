7e8ec3238ece555272e90bc0b3be872ad20362ac
umashankar <umashankarp33@gmail.com>
2024-01-21 16:55:06 +0530

    add few changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MdEditNote, MdOutlineToggleOff, MdDelete } from 'react-icons/md';
+import { IoIosAddCircle } from 'react-icons/io';
+import { BiSolidPaperPlane } from 'react-icons/bi';
+import { FaCheck } from 'react-icons/fa';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
+  const [editText, setEditText] = useState('');
+  const [editId, setEditId] = useState(null);
 
   useEffect(() => {
     axios
@@ -50,38 +57,95 @@ function App() {
     }
   };
 
+  const handleEdit = (todoId, currentText) => {
+    setEditText(currentText);
+    setEditId(todoId);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:5000/todos/${editId}`,
+        { text: editText }
+      );
+      setTodos(
+        todos.map((todo) => (todo._id === editId ? response.data : todo))
+      );
+      setEditText('');
+      setEditId(null);
+    } catch (error) {
+      console.error('Error updating todo:', error);
+    }
+  };
+
   return (
-    <div className='container'>
-      <h1>Todo App</h1>
-      <input
-        type='text'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo._id}>
-            <span
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none',
-              }}
-            >
-              {todo.text}
-            </span>
-            <button
-              onClick={() => markTodoCompleted(todo._id)}
-              className='toggle-btn'
-            >
-              Toggle Completed
-            </button>
-            <button onClick={() => deleteTodo(todo._id)} className='delete-btn'>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className='container'>
+        <h1 style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'1rem'}}>
+          <BiSolidPaperPlane />
+          Todo App
+        </h1>
+        <div className='input-container'>
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={addTodo}>
+            <IoIosAddCircle className='btn btn-add' />
+          </button>
+        </div>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo._id}>
+              {editId === todo._id ? (
+                <>
+                  <div className='edit-container'>
+                    <input
+                      type='text'
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                    />
+                    <button onClick={handleUpdate}>
+                      <FaCheck className='btn btn-check' />
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <span
+                    style={{
+                      textDecoration: todo.completed ? 'line-through' : 'none',
+                      width: '50%',
+                      textTransform: 'capitalize',
+                      paddingLeft: '3rem',
+                    }}
+                  >
+                    {todo.text}
+                  </span>
+                  <button onClick={() => handleEdit(todo._id, todo.text)}>
+                    <MdEditNote className='btn btn-edit' />
+                  </button>
+                  <button
+                    onClick={() => markTodoCompleted(todo._id)}
+                    className='toggle-btn'
+                  >
+                    <MdOutlineToggleOff className='btn btn-toggle' />
+                  </button>
+                  <button
+                    onClick={() => deleteTodo(todo._id)}
+                    className='delete-btn'
+                  >
+                    <MdDelete className='btn btn-delete' />
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>
+            Umashankar &copy; Copyright 2020-2024. All rights reserved.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
